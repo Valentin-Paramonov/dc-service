dcc04f46e5bbe8790be3d91483d7d59266fb2335
valentine <valentin.paramonov@protonmail.com>
2017-08-01 21:59:42 +0300

    Rename entry point class

--- src/main/java/paramonov/valentine/dcservice/App.java
+++ src/main/java/paramonov/valentine/dcservice/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DcserviceApplication {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
