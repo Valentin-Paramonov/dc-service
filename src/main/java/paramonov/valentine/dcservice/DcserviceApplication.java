8dfc933a7eaef8d7ee66635187ee4c0b378b4a7c
valentine <valentin.paramonov@protonmail.com>
2017-08-01 18:03:22 +0300

    Add template project

---
+++ src/main/java/paramonov/valentine/dcservice/DcserviceApplication.java
@@ -0,0 +1,12 @@
+package paramonov.valentine.dcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcserviceApplication.class, args);
+	}
+}
