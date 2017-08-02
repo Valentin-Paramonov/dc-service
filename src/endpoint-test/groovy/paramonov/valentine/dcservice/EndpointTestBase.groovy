bc0b0fb715224f9c3743a5675a2d3ca17d82bc10
valentine <valentin.paramonov@protonmail.com>
2017-08-02 22:22:54 +0300

    Use autoconfigurable MockMvc

--- src/endpoint-test/groovy/paramonov/valentine/dcservice/EndpointTestBase.groovy
+++ src/endpoint-test/groovy/paramonov/valentine/dcservice/EndpointTestBase.groovy
@@ -1,28 +1,21 @@
 package paramonov.valentine.dcservice
 
 import groovy.json.JsonBuilder
-import org.junit.Before
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(ConfigurationProfile.test)
+@AutoConfigureMockMvc
 abstract class EndpointTestBase {
     @Autowired
-    private WebApplicationContext context
     private MockMvc mvc
-
-    @Before
-    void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(context).build()
-    }
 
     protected mvc() {
         mvc
